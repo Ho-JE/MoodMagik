e478abf6efcbc82b31bcdb2bf1992fe5b61db74e
Ho-JE <juayen.ho.2020@scis.smu.edu.sg>
2023-03-04 22:03:20 +0800

    NO BAR

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,8 +3,6 @@ package com.example.myapplication
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -16,10 +14,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var recordingsArray: ArrayList<Recordings>
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
 
 
 
@@ -28,23 +26,23 @@ class MainActivity : AppCompatActivity() {
         newRecyclerview.setHasFixedSize(true)
 
         recordingsArray = arrayListOf<Recordings>()
-        val fakeRecording = Recordings("My recording",2.20,"15 Feb 2022")
+        val fakeRecording = Recordings("My recording", 2.20, "15 Feb 2022")
         recordingsArray.add(fakeRecording)
         recordingsArray.add(fakeRecording)
 
 
         var adapter = MyAdapter(recordingsArray)
         newRecyclerview.adapter = adapter
-        adapter.setOnItemClickListener(object: MyAdapter.onitemClickListener{
+        adapter.setOnItemClickListener(object : MyAdapter.onitemClickListener {
             override fun onItemClick(position: Int) {
                 val name = recordingsArray[position].name
                 val duration = recordingsArray[position].duration
                 val date = recordingsArray[position].date
 
-                val i = Intent(this@MainActivity,RecordingInfo::class.java)
-                i.putExtra("name",name)
-                i.putExtra("duration",duration)
-                i.putExtra("date",date)
+                val i = Intent(this@MainActivity, RecordingInfo::class.java)
+                i.putExtra("name", name)
+                i.putExtra("duration", duration)
+                i.putExtra("date", date)
                 startActivity(i)
             }
 
