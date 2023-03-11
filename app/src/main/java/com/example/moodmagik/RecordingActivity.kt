01c54cce2b1d2766eed7d2ce3894dce0d2118997
deadline2359 <89028059+deadline2359@users.noreply.github.com>
2023-03-12 03:45:18 +0800

    Recording Page for Tryout

---
+++ app/src/main/java/com/example/moodmagik/RecordingActivity.kt
@@ -0,0 +1,13 @@
+package com.example.moodmagik
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RecordingActivity  : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.recording_screen)
+
+
+    }
+}
