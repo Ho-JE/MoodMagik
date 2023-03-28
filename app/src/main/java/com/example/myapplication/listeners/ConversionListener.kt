14a16bd456ec5042306675732e0ef953e8b5d9a9
jenniferwxe11 <xewang.2020@smu.edu.sg>
2023-03-28 16:51:54 +0800

    Revert "changes"
    This reverts commit 917f2635c77be75a330cc29c9da1f4e5399c3527.

--- app/src/main/java/com/example/myapplication/listeners/ConversionListener.kt
+++
@@ -1,7 +0,0 @@
-package com.example.myapplication.listeners
-
-import com.example.myapplication.models.User
-
-interface ConversionListener {
-    fun onConversionClicked(user: User?)
-}
--- app/src/main/java/com/example/myapplication/listeners/UserListener.kt
+++
@@ -1,7 +0,0 @@
-package com.example.myapplication.listeners
-
-import com.example.myapplication.models.User
-
-interface UserListener {
-    fun onUserClicked(user: User?)
-}
